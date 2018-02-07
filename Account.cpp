0d909eed1a0dbea7feed67d3e1f65d6445c89f0e
bruv12 <bruv12@users.noreply.github.com>
2018-02-07 11:54:34 -0400

    dummy.h

---
+++ Account.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+#include<vector>
+#include"dummy.h"
+using namespace std;
+
+
+int main(){
+
+vector<Account>info;
+string Name, Balance;
+Account a;
+int x:
+
+ cin>> Name>>Balance>>endl;
+
+WHILE (Name && Balance <6){
+a.setName(Name);
+a.setBalance(Balance);
+info.push_back(a)
+}
+for(x=0; x<6;x++)
+{
+ cout<< info[x] .getName();
+ cout<< info[x] .getBalance();
+ }
+}
---
+++ dummy.h
@@ -0,0 +1,32 @@
+#include <string>
+using namespace std;
+
+class Account{ 
+               public:
+                      Account(string AccountName, doubleBalance)
+                     {
+                        name=Name;
+                        balance=Balance;
+
+                     }
+
+                  void setName (String AccountName){
+                         AccountName=Name
+                         }
+
+                   void setBalance(doubleBalance){
+                         AccountBalance=Balance
+                         }
+
+                  string getName() const {
+                         return AccountName
+                         }
+
+                   double getBalance() const {
+                          return AccountBalance;
+                         }
+               private:
+                         string AccountName,double AccoountBalance
+ }
+
+
